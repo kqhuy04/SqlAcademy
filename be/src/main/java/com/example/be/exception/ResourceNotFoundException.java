4106308abe058c2b8633c7b6631289c87cf975e2
kqhuy04 <khongquanghuy.ai@gmail.com>
2026-08-16 00:09:25 +0700

    feat: add simple CRUD mock endpoint

--- be/src/main/java/com/example/be/exception/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.be.exception;
-
-public class ResourceNotFoundException extends @org.jetbrains.annotations.NotNull X {
-    public ResourceNotFoundException(String s) {
-    }
-}
